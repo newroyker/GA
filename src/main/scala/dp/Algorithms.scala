01061539e90307a6348b50a3e4f661aa10438a0f
newroyker <newroyker@gmail.com>
2018-12-08 19:09:29 -0500

    refactor

---
+++ src/main/scala/dp/Algorithms.scala
@@ -0,0 +1,18 @@
+package dp
+
+object Algorithms {
+    def fibonacci(n: Int): Long = if(n < 0) 1L else 
+        (1 until n)
+            .foldLeft((1L,1L)){case ((li_2, li_1), _) => (li_1, li_1 + li_2)}
+            ._2
+
+    def lis(as: Seq[Int]): Int = {
+        val ls = as.indices.foldLeft(Seq.empty[Int]){ case (lis, i) => 
+            val max: Int = (0 to i).foldLeft(1){ case (m, j) => 
+                if(as(j) < as(i) && m < (lis(j)+1)) lis(j)+1 else m
+            }
+            lis :+ max
+        }
+        ls.max
+    }
+}
---
+++ src/test/scala/dp/AlgorithmsSpec.scala
@@ -0,0 +1,17 @@
+package dp
+
+import org.scalatest._
+import Algorithms._
+
+class AlgorithmsSpec extends FlatSpec with MustMatchers {
+    "fibonacci" should "return fib(n) where n = [0,7]" in {
+        val tests: Map[Int, Long] = Map(0 -> 1L, 1 -> 1L, 2 -> 2L, 3 -> 3L, 4 -> 5L, 5 -> 8L, 6 -> 13L, 7 -> 21L)
+        tests.foreach{
+            case (k,v) => fibonacci(k) must be (v)
+        }
+    }
+
+    "lis" should "return longest increasing sub sequence" in {
+        lis(Seq(5, 7, 4, -3, 9, 1, 10, 4, 5, 8, 9, 3)) must be (6)
+    }
+}
